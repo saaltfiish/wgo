b2804c8c460bd742edc56b0d43555a993016ac2a
Odin Lee <odinmanlee@gmail.com>
2017-11-08 11:38:10 +0800

    add type rest.Checklist

--- rest/db.go
+++ rest/db.go
@@ -4,6 +4,7 @@ package rest
 import (
 	"database/sql"
 	"encoding/json"
+	"math/bits"
 
 	"gorp"
 )
@@ -32,4 +33,53 @@ func (a *Array) FromDb(target interface{}) (gorp.CustomScanner, bool) {
 		return nil
 	}
 	return gorp.CustomScanner{new(sql.NullString), target, binder}, true
+}
+
+// checklist, 按位记录状态
+type Checklist map[int]bool
+
+func (cl Checklist) ToDb() (interface{}, error) {
+	sn := 0
+	for offset, t := range cl {
+		if t {
+			sn = sn | (1 << offset)
+		} else {
+			sn = sn &^ (1 << offset)
+		}
+	}
+	return sn, nil
+}
+
+func (cl Checklist) FromDb(target interface{}) (gorp.CustomScanner, bool) {
+	binder := func(holder, target interface{}) error {
+		sn := 0
+		if holder.(*sql.NullString).Valid {
+			sns := holder.(*sql.NullString).String
+			sn, _ = strconv.Atoi(sns)
+		}
+		ncl := make(Checklist)
+		for offset := 0; offset < bits.Len(uint(sn)); offset++ {
+			if sn&(1<<uint(offset)) > 0 {
+				ncl[offset] = true
+			} else {
+				ncl[offset] = false
+			}
+		}
+		*(target.(*Checklist)) = ncl
+		return nil
+	}
+	return gorp.CustomScanner{new(sql.NullString), target, binder}, true
+}
+
+// translate
+func (cl Checklist) Translate(seq map[int]string) map[string]bool {
+	tcl := make(map[string]bool)
+	for offset, name := range seq {
+		if t, ok := cl[offset]; ok {
+			tcl[name] = t
+		} else {
+			tcl[name] = false
+		}
+	}
+	return tcl
 }
