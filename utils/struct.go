31550910bcddd26424da9981a5f64d101855f0b2
Odin Lee <odinmanlee@gmail.com>
2018-06-05 12:49:07 +0800

    update

--- utils/struct.go
+++ utils/struct.go
@@ -527,6 +527,7 @@ func StringMap(i interface{}, opts ...string) map[string]string {
 			fn := field.Tags[tag].Name
 			fv := FieldByIndex(v, field.Index)
 			options := field.Tags[tag].Options
+			fmt.Printf("filed: %s, value: %+v", fn, fv)
 			fts := fv.Type().String()
 			fmt.Printf("field name: %s, value: %+v, type: %s\n", fn, fv, fts)
 			if (must == "" || options.Contains(must)) && (!options.Contains("omitempty") || !IsZero(fv)) {
