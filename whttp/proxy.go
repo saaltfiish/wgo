21231777041c9c2950a578573863905a489eb3aa
Odin Lee <odinmanlee@gmail.com>
2018-09-26 17:32:26 +0800

    chore: remove redun info

--- whttp/proxy.go
+++ whttp/proxy.go
@@ -340,7 +340,7 @@ func (rp *ReverseProxy) doProxy(c Context) error {
 	}
 
 	// add X-Forwarded-Proto
-	c.Error("add X-Forwarded-Proto: %s", c.Scheme())
+	// c.Error("add X-Forwarded-Proto: %s", c.Scheme())
 	outreq.Header.Set(HeaderXForwardedProto, c.Scheme())
 
 	res, err := rp.Transport.RoundTrip(outreq)
