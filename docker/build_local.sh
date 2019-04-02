f91f89979219c2570f0c3dade58ccedbeea41970
Odin Lee <odinmanlee@gmail.com>
2019-04-02 11:29:38 +0800

    feat: support modules

--- docker/build_local.sh
+++ docker/build_local.sh
@@ -1,3 +1,13 @@
-#!/bin/sh
+#! /bin/sh
+#
+# build_local.sh
+# Copyright (C) 2019 Odin <Odin@Odin-Pro.local>
+#
+# Distributed under terms of the MIT license.
+#
+# 在wgo:basic的基础上, 用于建立wgo的本地image
+# 本地测试用, 无需push
+
 docker build --rm=true -f docker/Dockerfile.dev -t 127.0.0.1:5001/arch/wgo:modules .
+
 docker push 127.0.0.1:5001/arch/wgo:modules
