6a8463fc82035f821694b6e56d3e7ba12502ace2
Odin Lee <odinmanlee@gmail.com>
2019-04-08 13:54:03 +0800

    feat: utils.Bind(set value to struct by field match)

--- utils/struct.go
+++ utils/struct.go
@@ -3,6 +3,7 @@ package utils
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"reflect"
 	"regexp"
@@ -604,4 +605,154 @@ func Convert(i interface{}, o interface{}) (interface{}, error) {
 		return nil, err
 	}
 	return o, nil
+}
+
+func Bind(ptr interface{}, data map[string]interface{}) error {
+	typ := reflect.TypeOf(ptr).Elem()
+	val := reflect.ValueOf(ptr).Elem()
+
+	if typ.Kind() != reflect.Struct {
+		return errors.New("binding element must be a struct")
+	}
+
+	for i := 0; i < typ.NumField(); i++ {
+		typeField := typ.Field(i)
+		structField := val.Field(i)
+		if !structField.CanSet() || structField.Kind() == reflect.Struct {
+			// not bind struct field, for now
+			continue
+		}
+		//if structField.Kind() == reflect.Struct {
+		//	err := Bind(structField.Addr().Interface(), data)
+		//	if err != nil {
+		//		return err
+		//	}
+		//	continue
+		//}
+		// first check field name
+		inputValue, exists := data[typeField.Name]
+		// second check field name(ignorecase)
+		if !exists {
+			inputValue, exists = data[strings.ToLower(typeField.Name)]
+		}
+		// third check field name(Underscore)
+		if !exists {
+			inputValue, exists = data[Underscore(typeField.Name)]
+		}
+		// forth check tag `json` name
+		if !exists {
+			inputFieldName := typeField.Tag.Get("json")
+			inputValue, exists = data[inputFieldName]
+		}
+		if !exists {
+			inputFieldName := typeField.Tag.Get("json")
+			inputValue, exists = data[inputFieldName]
+		}
+		// not found field to set
+		if !exists {
+			continue
+		}
+
+		if err := setWithProperType(typeField.Type.Kind(), inputValue, structField); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func setWithProperType(valueKind reflect.Kind, vi interface{}, structField reflect.Value) error {
+	val := ""
+	switch pv := vi.(type) {
+	case string:
+		val = pv
+	case *string:
+		val = *pv
+	case int64:
+		val = strconv.FormatInt(pv, 10)
+	case int:
+		val = strconv.FormatInt(int64(pv), 10)
+	case *int64:
+		val = strconv.FormatInt(*pv, 10)
+	case *int:
+		val = strconv.FormatInt(int64(*pv), 10)
+	default:
+		return errors.New("unknown support type")
+	}
+	switch valueKind {
+	case reflect.Int:
+		return setIntField(val, 0, structField)
+	case reflect.Int8:
+		return setIntField(val, 8, structField)
+	case reflect.Int16:
+		return setIntField(val, 16, structField)
+	case reflect.Int32:
+		return setIntField(val, 32, structField)
+	case reflect.Int64:
+		return setIntField(val, 64, structField)
+	case reflect.Uint:
+		return setUintField(val, 0, structField)
+	case reflect.Uint8:
+		return setUintField(val, 8, structField)
+	case reflect.Uint16:
+		return setUintField(val, 16, structField)
+	case reflect.Uint32:
+		return setUintField(val, 32, structField)
+	case reflect.Uint64:
+		return setUintField(val, 64, structField)
+	case reflect.Bool:
+		return setBoolField(val, structField)
+	case reflect.Float32:
+		return setFloatField(val, 32, structField)
+	case reflect.Float64:
+		return setFloatField(val, 64, structField)
+	case reflect.String:
+		structField.SetString(val)
+	default:
+		return errors.New("unknown type")
+	}
+	return nil
+}
+
+func setIntField(value string, bitSize int, field reflect.Value) error {
+	if value == "" {
+		value = "0"
+	}
+	intVal, err := strconv.ParseInt(value, 10, bitSize)
+	if err == nil {
+		field.SetInt(intVal)
+	}
+	return err
+}
+
+func setUintField(value string, bitSize int, field reflect.Value) error {
+	if value == "" {
+		value = "0"
+	}
+	uintVal, err := strconv.ParseUint(value, 10, bitSize)
+	if err == nil {
+		field.SetUint(uintVal)
+	}
+	return err
+}
+
+func setBoolField(value string, field reflect.Value) error {
+	if value == "" {
+		value = "false"
+	}
+	boolVal, err := strconv.ParseBool(value)
+	if err == nil {
+		field.SetBool(boolVal)
+	}
+	return err
+}
+
+func setFloatField(value string, bitSize int, field reflect.Value) error {
+	if value == "" {
+		value = "0.0"
+	}
+	floatVal, err := strconv.ParseFloat(value, bitSize)
+	if err == nil {
+		field.SetFloat(floatVal)
+	}
+	return err
 }
