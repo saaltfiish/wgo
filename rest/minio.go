4b2791a9680ac8d734f10fc11ffbbd4f1aaf9700
Odin Lee <odinmanlee@gmail.com>
2019-04-18 16:38:00 +0800

    feat: rest.PutPicByUrl

--- rest/minio.go
+++ rest/minio.go
@@ -8,8 +8,15 @@
 package rest
 
 import (
+	"bytes"
+	"crypto/md5"
+	"errors"
 	"fmt"
 	"io"
+	"strings"
+
+	"wgo/resty"
+	"wgo/whttp"
 
 	minio "github.com/minio/minio-go"
 	"github.com/minio/minio-go/pkg/policy"
@@ -41,6 +48,52 @@ func openObjectStorage() error {
 		// }
 	}
 	return nil
+}
+
+func fileExtByMimeType(mt string) string {
+	switch strings.ToLower(mt) {
+	case "image/png":
+		return ".png"
+	case "image/jpeg", "image/jpg":
+		return ".jpg"
+	case "image/gif":
+		return ".gif"
+	default:
+		return ""
+	}
+}
+
+func PutPicByUrl(url, bucketName string) (string, error) {
+	objectStorage, err := NewObjectStorage(mio["endpoint"].(string), mio["access_key"].(string), mio["secret_key"].(string), mio["secure"].(bool))
+	if err != nil {
+		return "", err
+	}
+	path, err := objectStorage.PutPicByUrl(url, bucketName)
+	if err != nil {
+		return "", err
+	}
+	return fmt.Sprintf("%s%s", mio["cdn"].(string), path), nil
+
+}
+
+// put object by url
+func (o *ObjectStorage) PutPicByUrl(url, bucketName string) (string, error) {
+	// download pic
+	resty.SetRedirectPolicy(resty.FlexibleRedirectPolicy(15))
+	if picRes, err := resty.R().Get(url); err == nil {
+		mimeType := picRes.Header().Get(whttp.HeaderContentType)
+		body := picRes.Body()
+		checksum := fmt.Sprintf("%x", md5.Sum(body))
+		objectName := fmt.Sprintf("%s/%s/%s%s", checksum[0:2], checksum[2:4], checksum, fileExtByMimeType(mimeType))
+		Debug("[PutPicByUrl]objectName: %s, checksum: %s, mimeType: %s, url: %s", objectName, checksum, mimeType, url)
+		// return PutObject(bucketName, objectName, mimeType, bytes.NewReader(body), int64(len(body)))
+		if _, err := o.PutObject(bucketName, objectName, mimeType, bytes.NewReader(body), int64(len(body))); err != nil {
+			// Debug("[PutPicByUrl]upload image failed: %s", err)
+			return "", err
+		}
+		return fmt.Sprintf("/%s/%s", bucketName, objectName), nil
+	}
+	return "", errors.New("[PutPicByUrl]failed")
 }
 
 // set policy
