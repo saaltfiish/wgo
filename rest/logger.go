79106c07b4b3663c36f224d7ef789163ee72371d
Odin Lee <odinmanlee@gmail.com>
2019-05-11 15:09:47 +0800

    fixed: logger issue

--- rest/logger.go
+++ rest/logger.go
@@ -60,36 +60,36 @@ func Error(arg0 interface{}, args ...interface{}) {
 
 // logging
 // Debug
-func (rest *REST) Debug(arg0 interface{}, args ...interface{}) {
-	if rest.Context() != nil {
-		rest.Context().Debug(arg0, args...)
+func (r *REST) Debug(arg0 interface{}, args ...interface{}) {
+	if r != nil && r.Context() != nil {
+		r.Context().Debug(arg0, args...)
 	} else {
 		wgo.Debug(arg0, args...)
 	}
 }
 
 // Info
-func (rest *REST) Info(arg0 interface{}, args ...interface{}) {
-	if rest.Context() != nil {
-		rest.Context().Info(arg0, args...)
+func (r *REST) Info(arg0 interface{}, args ...interface{}) {
+	if r != nil && r.Context() != nil {
+		r.Context().Info(arg0, args...)
 	} else {
 		wgo.Info(arg0, args...)
 	}
 }
 
 // Warn
-func (rest *REST) Warn(arg0 interface{}, args ...interface{}) {
-	if rest.Context() != nil {
-		rest.Context().Warn(arg0, args...)
+func (r *REST) Warn(arg0 interface{}, args ...interface{}) {
+	if r != nil && r.Context() != nil {
+		r.Context().Warn(arg0, args...)
 	} else {
 		wgo.Warn(arg0, args...)
 	}
 }
 
 // Error
-func (rest *REST) Error(arg0 interface{}, args ...interface{}) {
-	if rest.Context() != nil {
-		rest.Context().Error(arg0, args...)
+func (r *REST) Error(arg0 interface{}, args ...interface{}) {
+	if r != nil && r.Context() != nil {
+		r.Context().Error(arg0, args...)
 	} else {
 		wgo.Error(arg0, args...)
 	}
