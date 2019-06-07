0ba6ea1b3ea12a601a9f27d2243bd2c59f3b1ef9
Odin Lee <odinmanlee@gmail.com>
2019-06-07 15:37:25 +0800

    cicd: [skip ci]build_local

--- docker/build_local.sh
+++ docker/build_local.sh
@@ -8,6 +8,6 @@
 # 在wgo:basic的基础上, 用于建立wgo的本地image
 # 本地测试用, 无需push
 
-docker build --rm=true -f docker/Dockerfile.dev -t 127.0.0.1:5001/arch/wgo:modules .
+docker build --rm=true -f docker/Dockerfile.dev -t 127.0.0.1:5001/arch/wgo:latest .
 
-docker push 127.0.0.1:5001/arch/wgo:modules
+docker push 127.0.0.1:5001/arch/wgo:latest
